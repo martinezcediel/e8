3ce60a2e641e27e4dcf0b9ad16c8cc608cd54cb5
martinezcediel <alvaro.martinez.algora@gmail.com>
2022-03-02 14:58:45 +0100

    ola

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    public Material[] materials;
+    public Renderer Rend;
+
+    private int index = 1;
+
+    public void buttonPressed()
+    {
+        if (materials.Length == 0)
+            return;
+
+        index += 1;
+
+        if (index == materials.Length + 1)
+            index = 1;
+
+        print(index);
+
+        Rend.sharedMaterial = materials[index - 1];
+
+    }
+
+}
